f6d7588e89279cad4e9de74b68e82e7bb48ae743
Eduardo Veras <eduardo.storm@gmail.com>
2018-09-03 16:15:13 -0400

    Adding basic vue-material

---
+++ src/components/Head.vue
@@ -0,0 +1,20 @@
+<template>
+<div>
+    <md-toolbar class="md-accent" md-elevation="1">
+      <h3 class="md-title" style="flex: 1">Title</h3>
+      <md-button>Refresh</md-button>
+      <md-button class="md-primary">Create</md-button>
+    </md-toolbar>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Head',
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
